30b5f6ec5f3a353531689e361c5a4be6b6832e47
the-aroma-of-street-food <3862350410@qq.com>
2026-04-25 13:58:26 +0800

    更新：驱动单摄像头及显示

--- Core/Src/MLX90640_HW_I2C.c
+++ Core/Src/MLX90640_HW_I2C.c
@@ -2,7 +2,7 @@
 #include "i2c.h"             
 
 // 1. 声明外部的动态 I2C 指针（这个指针将在 main.c 中定义和切换）
-extern I2C_HandleTypeDef* active_hi2c; 
+//extern I2C_HandleTypeDef* active_hi2c; 
 
 // =========================================================================
 // I2C 初始化与复位 (保持不变)
@@ -18,7 +18,7 @@ int MLX90640_I2CRead(uint8_t slaveAddr, uint16_t startAddress, uint16_t nMemAddr
     uint8_t i2c_addr = slaveAddr << 1; 
 
     // 2. 将 &hi2c1 替换为 active_hi2c
-    if(HAL_I2C_Mem_Read(active_hi2c, i2c_addr, startAddress, I2C_MEMADD_SIZE_16BIT, (uint8_t*)data, nMemAddressRead * 2, 500) == HAL_OK)
+    if(HAL_I2C_Mem_Read(&hi2c1, i2c_addr, startAddress, I2C_MEMADD_SIZE_16BIT, (uint8_t*)data, nMemAddressRead * 2, 500) == HAL_OK)
     {
         for(int i = 0; i < nMemAddressRead; i++)
         {
@@ -39,7 +39,7 @@ int MLX90640_I2CWrite(uint8_t slaveAddr, uint16_t writeAddress, uint16_t data)
     uint16_t data_swapped = (data >> 8) | (data << 8);
 
     // 3. 将 &hi2c1 替换为 active_hi2c
-    if(HAL_I2C_Mem_Write(active_hi2c, i2c_addr, writeAddress, I2C_MEMADD_SIZE_16BIT, (uint8_t*)&data_swapped, 2, 500) == HAL_OK)
+    if(HAL_I2C_Mem_Write(&hi2c1, i2c_addr, writeAddress, I2C_MEMADD_SIZE_16BIT, (uint8_t*)&data_swapped, 2, 500) == HAL_OK)
     {
         return 0; 
     }
